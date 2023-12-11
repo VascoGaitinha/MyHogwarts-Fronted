9b44e6472efc251f4b9022e3aaa6f29425d74c76
Vasco Gaitinha <vascomiguel.gaitinha@gmail.com>
2023-12-11 11:27:55 +0000

    first push fixed loggin from template

--- src/Context/auth.context.jsx
+++ src/Context/auth.context.jsx
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const API_URL = "http://localhost:5005";
 
@@ -9,6 +10,7 @@ function AuthProviderWrapper(props){
     const [isLoggedIn, setIsLoggedIn] = useState(false);
     const [isLoading, setIsLoading] = useState(true);
     const [user, setUser] = useState(null);
+    const navigate = useNavigate();
     
     /* Store the token in the local storage */
     const storeToken = (token) =>{
@@ -46,6 +48,7 @@ function AuthProviderWrapper(props){
     }
 
     const logOut = () =>{
+        navigate('/')
         removeToken();
         authenticateUser();
     }
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,13 +1,24 @@
 import { useContext } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../Context/auth.context";
 
 function HomePage() {
   const { isLoggedIn, user, logOut } = useContext(AuthContext);
-  console.log(user)
+  const navigate = useNavigate();
+
+
+
   return (
+
     <div>
-      <h1>Welcome {user.name} </h1>
+      {!user || !isLoggedIn ?
+      <div>
+        <h1>Please Log In</h1>
+        <button onClick={()=>{navigate('/login')}}>Login</button>
+      </div>
+      :
+      <h1>Welcome {user.name} </h1>}
+      <button onClick={logOut}>Log Out</button>
     </div>
   );
 }
